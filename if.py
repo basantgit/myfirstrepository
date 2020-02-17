9cf76f0e94c786df342b4da9021f7a1bce98fc0d
basantgit <basantmohanty7@gmail.com>
2020-02-17 01:43:59 -0600

    if.py

---
+++ if.py
@@ -0,0 +1,7 @@
+#while loop
+name=""
+pwd=""
+while name!="basant" or pwd!="mohanty":
+  name=input("Enter Name:")
+  pwd=input("Enter Password:")
+print("Thanks for correct name and passwd entering")
