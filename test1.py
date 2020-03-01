1f0278b5130ca22117acd216ee40bb3c9e2f0d26
basantgit <basantmohanty7@gmail.com>
2020-02-29 23:12:30 -0600

    test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+city=input('Enter your city name')
+list=["hyderabad","pune","balasore","kolkata"]
+if city.rstrip() in list:
+  print("your city is available and customer care number is 8895569510","address:orissa",
+"pin:756042","dist-balasore","state-orissa")
+else:
+  print(" Not available...Please enter a valid city name")
