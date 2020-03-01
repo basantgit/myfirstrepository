aa0e55cc01875d62bb25f53fa2e7dd250df7c0f8
Basant Kumar Mohanty <basantmohanty7@gmail.com>
2020-03-01 01:28:10 -0600

    Update if.py

--- if.py
+++ if.py
@@ -1,7 +1,3 @@
 #while loop
-name=""
-pwd=""
-while name!="basant" or pwd!="mohanty":
-  name=input("Enter Name:")
-  pwd=input("Enter Password:")
-print("Thanks for correct name and passwd entering")
+x=("my name is basant")
+print(x)
